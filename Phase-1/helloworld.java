376eb0c7a830dea27c2c91120fe09953fb9a90db
Kingshaikk <149676232+Kingshaikk@users.noreply.github.com>
2024-02-04 20:04:07 +0530

    Create helloworld.java

---
+++ Phase-1/helloworld.java
@@ -0,0 +1,21 @@
+// HelloWorldServlet.java
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HelloWorldServlet")
+public class HelloWorldServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h2>Hello, World!</h2>");
+        out.println("</body></html>");
+    }
+}
